4e1fe9418857e2e5643343554045f77c560d7c4e
RaviMauryaHootowl <ravimaurya027@gmail.com>
2021-03-23 21:07:43 +0530

    React+Flask Boilerplate setup

---
+++ chamakta-sikka-web/src/App.js
@@ -0,0 +1,35 @@
+import React, {useState, useEffect} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => {
+
+  const [currentTime, setCurrentTime] = useState(0);
+  useEffect(() => {
+    fetch('/time').then(res => res.json())
+      .then(data => {
+        setCurrentTime(data.time);
+      })
+  },[])
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Time is {currentTime}
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
